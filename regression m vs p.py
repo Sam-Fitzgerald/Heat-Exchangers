1b65c9d6fdca42db8a0a97c654d6c7c4fd0374fb
momopmoXZ <hellohihi.cb@gmail.com>
2024-05-14 15:44:55 +0100

    push

--- regression m vs p.py
+++ regression m vs p.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def cold():
-    cold_flowrate = [0.7083,0.6417,0.5750,0.5083,0.4250,0.3583,0.3083,0.2417,0.1917,0.1583]
-    cold_pressure_rise =[0.1310,0.2017,0.2750,0.3417,0.4038,0.4503,0.4856,0.5352,0.5717,0.5876]
+    cold_flowrate = [0.5776, 0.5597, 0.5152, 0.4626, 0.4203, 0.3568, 0.2717, 0.1870, 0.1444, 0.1024]
+    cold_pressure_rise =[0.1583, 0.1917, 0.2417, 0.3083, 0.3583, 0.4250, 0.5083, 0.5750, 0.6083, 0.6333]
+
     fit = np.polyfit(cold_flowrate, cold_pressure_rise, 2, rcond=None, full=False, w=None, cov=False)
     print(fit)
     x = np.linspace(0, 1, 100)
@@ -13,8 +14,8 @@ def cold():
 
 
 def hot():
-    hot_flowrate= [0.4722,0.4340, 0.3924, 0.3507, 0.3021, 0.2535, 0.1979, 0.1493, 0.1111, 0.0694] 
-    hot_pressure_rise =[0.0538,0.1192,0.1727,0.2270,0.2814,0.3366,0.3907,0.4456,0.4791,0.5115]
+    hot_flowrate= [0.4826, 0.4340, 0.3924, 0.3507, 0.3021, 0.2535, 0.1979, 0.1493, 0.1111, 0.0694] 
+    hot_pressure_rise =[0.0944, 0.1662, 0.2297, 0.2820, 0.3294, 0.3856, 0.4447, 0.5006, 0.5311, 0.5615]
     fit = np.polyfit(hot_flowrate, hot_pressure_rise, 2, rcond=None, full=False, w=None, cov=False)
     print(fit)
     x = np.linspace(0, 1, 100)
@@ -22,4 +23,9 @@ def hot():
     plt.plot(x,y)
     plt.show()
     return fit
+
+print(hot())
+print(cold())
+
+
 
